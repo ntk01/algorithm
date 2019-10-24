3d361821bc2a3e257f76b64f98a97a2934d85027
ntk01 <neuron.futuring1024@gmail.com>
2019-10-24 19:56:18 +0900

    test

--- BinarySearch/testBinarySearch.java
+++ BinarySearch/testBinarySearch.java
@@ -1,4 +1,13 @@
 package BinarySearch;
 
-public class testBinarySearch {
+public class testBinarySearch{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,5,8,10};
+        int v1 = 3;
+        int v2 = 6;
+        System.out.println(BinarySearch.lowerbound(arr, v1));   //output: 2
+        System.out.println(BinarySearch.lowerbound(arr, v2));   //output: 4
+        System.out.println(BinarySearch.upperbound(arr, v1));   //output: 3
+        System.out.println(BinarySearch.upperbound(arr, v2));   //output: 4
+    }
 }
