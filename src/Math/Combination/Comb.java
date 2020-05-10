857fb823554eb98b6ce0e5fbb4cee076d7a14988
ntk01 <n.taku.law@gmail.com>
2020-05-11 05:52:18 +0900

    add nCk2

--- src/Math/Combination/Comb.java
+++ src/Math/Combination/Comb.java
@@ -15,16 +15,30 @@ public class Comb {
         }
     }
 
-    public long modpow(long a, long b){  // 計算量はlog(MOD);
+    // O(log(MOD))
+    public long modpow(long a, long b){
         if (b==0) return 1;
         else if (b==1) return a;
         long x = modpow(a, b/2);
         return b%2 == 0 ? x * x % MOD : x * (x*a%MOD) % MOD;
     }
+
     public long inv(long n){ // 逆元。x^(-1)≡x^(p-2) (MOD p)
         return modpow(n, MOD-2);
     }
-    public long nck(int n, int k){
+
+    public long nCk1(int n, int k){
         return n < k ? 0: facts[n] * (inv(facts[n-k]) * inv(facts[k]) % MOD) % MOD;
     }
+
+    // O(k)
+    public long nCk2(int n, int k) {
+        long ret = 1;
+        for (int i=0; i<k; i++) {
+            ret = ret * (n - i) % MOD * inv(k - i);
+            ret %= MOD;
+        }
+        return ret % MOD;
+    }
+
 }
