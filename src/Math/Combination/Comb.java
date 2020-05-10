ab59101a8f4cc624c7175c9c7425727a00154f79
ntk01 <n.taku.law@gmail.com>
2020-05-11 05:25:31 +0900

    Combination

---
+++ src/Math/Combination/Comb.java
@@ -0,0 +1,30 @@
+package Math.Combination;
+
+public class Comb {
+    int n;   //　要素数
+    int MOD = (int)1e9+7;
+    long[] facts;
+
+    public Comb(int n, int MOD){
+        this.n = n;
+        this.MOD = MOD;
+        facts = new long[n+1];
+        facts[0] = 1;
+        for (int i=1; i<n+1; i++){
+            facts[i] = facts[i-1] * i % MOD;
+        }
+    }
+
+    public long modpow(long a, long b){  // 計算量はlog(MOD);
+        if (b==0) return 1;
+        else if (b==1) return a;
+        long x = modpow(a, b/2);
+        return b%2 == 0 ? x * x % MOD : x * (x*a%MOD) % MOD;
+    }
+    public long inv(long n){ // 逆元。x^(-1)≡x^(p-2) (MOD p)
+        return modpow(n, MOD-2);
+    }
+    public long nck(int n, int k){
+        return n < k ? 0: facts[n] * (inv(facts[n-k]) * inv(facts[k]) % MOD) % MOD;
+    }
+}
