56ed5bd9267b2ea333aa4c1a19c5e37214474e0b
ntk01 <n.taku.law@gmail.com>
2020-05-31 16:19:25 +0900

    add unite2 func

--- src/Tree/UnionFind/UnionFind.java
+++ src/Tree/UnionFind/UnionFind.java
@@ -2,33 +2,54 @@ package Tree.UnionFind;
 
 class UnionFind {
 
-    int[] parents, sizes;
+    int[] parent, rank, size;
+
+    void init(int n) {
+        for (int i=0; i<n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
 
     UnionFind(int n) {
-        parents = new int[n];
-        sizes = new int[n];
+        parent = new int[n];
+        size = new int[n];
         for (int i=0; i<n; i++) {
-            parents[i] = i;
-            sizes[i] = 1;
+            parent[i] = i;
+            size[i] = 1;
         }
     }
 
     public int find(int x) {
-        if (x==parents[x]) return x;
-        return parents[x] = find(parents[x]);
+        if (x==parent[x]) return x;
+        return parent[x] = find(parent[x]);
     }
 
-    public void unite(int x, int y) {
+//    結合して木のサイズを更新
+    public void unite1(int x, int y) {
         x = find(x);
         y = find(y);
         if (x==y) return;
-        if (sizes[x] < sizes[y]) {
+        if (size[x] < size[y]) {
             int tmp = x;
             x = y;
             y = tmp;
         }
-        parents[y] = x;
-        sizes[x] += sizes[y];
+        parent[y] = x;
+        size[x] += size[y];
+    }
+
+//    結合して木の深さを更新
+    public void unite2(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x==y) return;
+        if (rank[x] < rank[y]) {
+            parent[x] = y;
+        } else {
+            parent[y] = x;
+            if (rank[x]==rank[y]) rank[x]++;
+        }
     }
 
     public boolean same(int x, int y) {
@@ -36,7 +57,7 @@ class UnionFind {
     }
 
     public int size(int x) {
-        return sizes[find(x)];
+        return size[find(x)];
     }
 
 
