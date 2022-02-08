af33cbc9486915e5386538df25fbd447fac0aa1c
ntk01 <n.taku.law@gmail.com>
2022-02-08 23:43:43 +0900

    fix knapsack

--- src/DP/Knapsack.java
+++ src/DP/Knapsack.java
@@ -13,38 +13,30 @@ public class Knapsack {
 
         int[][] arr = new int[n][2];
 
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i][0] = sc.nextInt();
             arr[i][1] = sc.nextInt();
         }
 
-        int[][] dp = new int[n+1][w+1];
+        int[][] dp = new int[n + 1][w + 1];
 
-        for (int i=1; i<=n; i++) {
-            for (int j=1; j<=w; j++) {
-                if (arr[i-1][1]<=j) {
-                    if (arr[i-1][0]+dp[i-1][j-arr[i-1][1]]>dp[i-1][j]) {
-                        dp[i][j] = arr[i-1][0]+dp[i-1][j-arr[i-1][1]];
-                    }else {
-                        dp[i][j] = dp[i-1][j];
-                    }
-                }else {
-                    dp[i][j] = dp[i-1][j];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= w; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (arr[i - 1][1] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], arr[i - 1][0] + dp[i - 1][j - arr[i - 1][1]]);
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
-
-        for (int i=0; i<=n; i++) {
-            for (int j=0; j<w; j++) {
-                sb.append(dp[i][j]+" ");
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < w; j++) {
+                sb.append(dp[i][j] + " ");
             }
-            sb.append(dp[i][w]+"\n");
+            sb.append(dp[i][w] + "\n");
         }
-
-        System.out.print("DP table:\n"+sb.toString());
-
-        System.out.print("ans:\n"+dp[n][w]);
+        System.out.print("DP table:\n" + sb);
+        System.out.print("ans:\n" + dp[n][w]);
     }
 }
